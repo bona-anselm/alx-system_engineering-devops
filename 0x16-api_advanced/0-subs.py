6580ed78a25730015d922f144a001239a04db3d8
Bonaventure Anselm <bona.anselm@gmail.com>
2023-05-10 23:35:58 +0100

    Readme file & Task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""a function that queries the Reddit API and returns the number
+    of subscribers (not active users, total subscribers) for a given
+    subreddit
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+            "User-Agent": "Linux/1.0"
+            }
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        response = response.json()
+        subscribers = response.get('data').get('subscribers')
+        return subscribers
+
+    return 0
